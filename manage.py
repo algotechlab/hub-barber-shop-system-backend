460d0531b840d9d2c39eb5472357c6dbd4da7b3f
Hedris Pereira <hedris.pereira@eemovel.com>
2025-04-29 20:27:19 -0300

    fix: adjust middleware

--- manage.py
+++ manage.py
@@ -1,5 +1,21 @@
+from dotenv import load_dotenv
+
 from src.external import create_app
+from src.settings._base import config_by_name, flask_env
+
+load_dotenv()
+
+class Middleware:
+    def __init__(self, app):
+        self.app = app
+
+    def __call__(self, environ, start_response):
+        environ.update({"Id": 1})
+        environ.update({"email": "hrpbs@teste.com"})
+        return self.app(environ, start_response)
+
+app = create_app()
+app.wsgi_app = Middleware(app.wsgi_app)
 
 if __name__ == "__main__":
-    app = create_app()
-    app.run()
+    app.run(port=config_by_name[flask_env].PORT, debug=config_by_name[flask_env].DEBUG)
