8807c1e7d40e0807898e7d2537d53c09437a57df
Hedris Pereira <hedris.pereira@eemovel.com>
2026-01-20 16:16:09 -0300

    feat: implementando nova estrutura do projeto

--- src/auth/hash_compact.py
+++
@@ -1,8 +0,0 @@
-import hashlib
-
-
-class CompactHash:
-    @staticmethod
-    def compact_token(token: str) -> str:
-        """Generate a compact hash for the given JWT token."""
-        return hashlib.sha256(token.encode()).hexdigest()[:32]
