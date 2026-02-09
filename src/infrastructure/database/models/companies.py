2ecf214a744290beb466f96710d6f2b6ec2223cd
Hedris Pereira <hedrisgts@gmail.com>
2026-02-09 12:21:08 -0300

    feat: implementando o proprietario do sistema

--- src/infrastructure/database/models/companies.py
+++ src/infrastructure/database/models/companies.py
@@ -1,4 +1,6 @@
-from sqlalchemy import Boolean, String
+from uuid import UUID
+
+from sqlalchemy import Boolean, ForeignKey, String
 from sqlalchemy.orm import Mapped, mapped_column
 
 from src.infrastructure.database.models.base import BaseModel
@@ -8,3 +10,4 @@ class Company(BaseModel):
     name: Mapped[str] = mapped_column(String(255), nullable=False)
     slug: Mapped[str] = mapped_column(String(100), nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean, nullable=False, default=True)
+    owner_id: Mapped[UUID] = mapped_column(ForeignKey('owner.id'), nullable=False)
